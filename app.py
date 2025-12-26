02edead3b7dd957d7fac0dcdea5ba37a952b4590
unknown <your_email@example.com>
2025-12-26 13:52:24 +0530

    Add health check endpoint

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "HELLO MASTER! I STARTED DEVOPS 🚀"
 
+@app.route("/health")
+def health():
+    return {"status": "UP"}
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
